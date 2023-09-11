b87fd281b509718ace5948936489a4eb2b232537
Blitzd <36904558+BlitzdUK@users.noreply.github.com>
2023-09-12 00:23:45 +0100

    no

--- dev/geolocation/script.js
+++ dev/geolocation/script.js
@@ -1,80 +1,67 @@
-// Get references to HTML elements
-const table = document.querySelector('table');
-const mapDiv = document.getElementById('map');
+// Function to fetch GPS data
+function fetchGPSData() {
+    if ("geolocation" in navigator) {
+        navigator.geolocation.watchPosition((position) => {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            const speed = position.coords.speed || "N/A";
+            const altitude = position.coords.altitude || "N/A";
+            const accuracy = position.coords.accuracy || "N/A";
 
-// Initialize the map
-const mymap = L.map(mapDiv).setView([0, 0], 13);
+            // Display coordinates, speed, altitude, and accuracy
+            document.getElementById("coordinates").textContent = `Coordinates: ${latitude}, ${longitude}`;
+            document.getElementById("speed").textContent = `Speed: ${speed}`;
+            document.getElementById("altitude").textContent = `Altitude: ${altitude}`;
+            document.getElementById("accuracy").textContent = `GPS Accuracy: ${accuracy}`;
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-}).addTo(mymap);
+            // Fetch address using OpenWeatherMap API
+            fetch(`https://app.owm.io/app/1.1/geo/reverse?lat=${latitude}&lon=${longitude}&appid=dd8770c04bfc20b2f1885e5f1c901125&deviceid=011006FD-129A-458E-8A98-7283010A3558`)
+                .then((response) => response.json())
+                .then((data) => {
+                    const address = data.address || "N/A";
+                    document.getElementById("address").textContent = `Address: ${address}`;
 
-let previousCoords = null;
+                    // Check if the device has moved significantly (adjust threshold as needed)
+                    if (position.coords.accuracy <= 10) {
+                        // If the device has moved, post JSON response to the specified URL
+                        const postData = {
+                            latitude,
+                            longitude,
+                            speed,
+                            altitude,
+                            accuracy,
+                            address,
+                        };
 
-// Function to update the table and send data to the server
-function updateTableAndSendData(position) {
-    const { coords, timestamp } = position;
-    const { latitude, longitude, speed, altitude, accuracy } = coords;
-
-    // Check if the device has moved
-    if (previousCoords && previousCoords.latitude === latitude && previousCoords.longitude === longitude) {
-        return;
+                        fetch("https://static.blitzd.uk:1880/data/geolocation", {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                            body: JSON.stringify(postData),
+                        })
+                            .then((response) => {
+                                if (response.status === 200) {
+                                    console.log("Data posted successfully.");
+                                } else {
+                                    console.error("Error posting data:", response.statusText);
+                                }
+                            })
+                            .catch((error) => {
+                                console.error("Error:", error);
+                            });
+                    }
+                })
+                .catch((error) => {
+                    console.error("Error fetching address:", error);
+                });
+        }, (error) => {
+            console.error("Error getting geolocation:", error.message);
+        });
+    } else {
+        console.error("Geolocation is not available in this browser.");
     }
-
-    previousCoords = { latitude, longitude };
-
-    // Display coordinates, speed, altitude, and GPS accuracy in the table
-    const row = table.insertRow(1);
-    row.innerHTML = `
-        <td>${latitude.toFixed(6)}, ${longitude.toFixed(6)}</td>
-        <td>${speed ? speed.toFixed(2) : 'N/A'} m/s</td>
-        <td>${altitude ? altitude.toFixed(2) : 'N/A'} meters</td>
-        <td>${accuracy ? accuracy.toFixed(2) : 'N/A'} meters</td>
-    `;
-
-    // Create a marker on the map
-    const marker = L.marker([latitude, longitude]).addTo(mymap);
-    marker.bindPopup(`Speed: ${speed} m/s<br>Altitude: ${altitude} meters<br>GPS Accuracy: ${accuracy} meters`);
-    mymap.setView([latitude, longitude], 13);
-
-    // Send data to the server
-    fetch(`https://app.owm.io/app/1.1/geo/reverse?lat=${latitude}&lon=${longitude}&appid=dd8770c04bfc20b2f1885e5f1c901125&deviceid=011006FD-129A-458E-8A98-7283010A3558`)
-        .then(response => response.json())
-        .then(data => {
-            // Check if the device has moved again before sending the data
-            if (previousCoords && previousCoords.latitude === latitude && previousCoords.longitude === longitude) {
-                return;
-            }
-
-            // Send the data to the server using a POST request
-            fetch('https://static.blitzd.uk:1880/data/geolocation', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    timestamp,
-                    coordinates: { latitude, longitude },
-                    speed,
-                    altitude,
-                    accuracy,
-                    address: data.display_name,
-                }),
-            });
-        })
-        .catch(error => console.error('Error fetching data:', error));
 }
 
-// Start watching for position changes
-const watchId = navigator.geolocation.watchPosition(updateTableAndSendData, error => {
-    console.error('Error getting GPS data:', error);
-}, {
-    enableHighAccuracy: true,
-    maximumAge: 0,
-    timeout: 5000,
-});
-
-// Stop watching when the page is unloaded
-window.addEventListener('unload', () => {
-    navigator.geolocation.clearWatch(watchId);
-});
+// Call the fetchGPSData function when the page loads
+window.onload = fetchGPSData;
